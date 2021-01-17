abd5b6db4d90922dfae64553c516401f41724800
omd24 <jack34169>
2021-01-17 18:11:04 +0330

    dynarray helper

---
+++ math2d/Source.cpp
@@ -0,0 +1,75 @@
+/* ===========================================================
+   #File: Source.cpp #
+   #Date: 17 Jan 2021 #
+   #Revision: 1.0 #
+   #Creator: Omid Miresmaeili #
+   #Description: Exprimenting with math2d #
+   #Notice: (C) Copyright 2021 by Omid. All Rights Reserved. #
+   =========================================================== */
+
+#include "dynarray.h"
+
+int main () {
+    double * a = NULL;
+    DYN_ARRAY_INIT(double, a);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+
+    DYN_ARRAY_DEINIT(a);
+    return (0);
+}
+
