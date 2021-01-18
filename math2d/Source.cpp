9d6f28fa09e2ac22c98b708f5c8d7bc80358e5d5
omd24 <jack34169>
2021-01-18 12:09:33 +0330

    fixed printf mismatch for x64 build

--- math2d/Source.cpp
+++ math2d/Source.cpp
@@ -13,61 +13,61 @@ int main () {
     double * a = NULL;
     DYN_ARRAY_INIT(double, a);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
-
-
-    DYN_ARRAY_PUSHBACK(double, a, 25);
-
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
 
     DYN_ARRAY_PUSHBACK(double, a, 25);
 
-    ::printf("the size of the array = %d\n", DYN_ARRAY_LENGTH(a));
-    ::printf("the capcaity of the array = %d\n", DYN_ARRAY_CAPACITY(a));
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
+
+
+    DYN_ARRAY_PUSHBACK(double, a, 25);
+
+    ::printf("the size of the array = %I64u\n", DYN_ARRAY_LENGTH(a));
+    ::printf("the capcaity of the array = %I64u\n", DYN_ARRAY_CAPACITY(a));
 
     DYN_ARRAY_DEINIT(a);
     return (0);
